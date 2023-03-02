70b1e206376175f5f485a8e30f70adb6ab0e50a6
eastevil <xiao.hu@yunkaiwangluo.com>
2023-03-02 14:58:21 +0800

    修改nuwa版本号为1.0.1

--- app/src/main/java/com/east/evil/myth/huangdi/MainActivity.kt
+++ app/src/main/java/com/east/evil/myth/huangdi/MainActivity.kt
@@ -2,10 +2,16 @@ package com.east.evil.myth.huangdi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.star.starlight.ui.view.commons.RichAdapter
+import com.star.starlight.ui.view.commons.RichBuilder
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = RichBuilder(this)
+            .builder();
+
     }
 }
